4bd087336f0292a2dd8afcd2eb897ffc08dc1674
salmankha19 <salmankha@hotmail.com>
2026-07-07 00:43:06 +0100

    chore(release): snapshot before event-queue fixes

--- components/chronolink_hal/src/hal_init.c
+++ components/chronolink_hal/src/hal_init.c
@@ -5,11 +5,20 @@
 #include "hal_rtc.h"
 #include "hal_display.h"
 #include "hal_sensors.h"
+#include "pdl_compat.h"
+#include "esp_log.h"
+
+static const char *TAG = "HAL_INIT";
 
 /* Simple initializer: call sub-inits and return success.
    If you later change sub-inits to return status, switch to checking them. */
 hal_status_t HAL_Init(void)
 {
+    if (pdl_board_init() != ESP_OK) {
+        ESP_LOGE(TAG, "pdl_board_init failed");
+        return HAL_ERR_INIT;
+    }
+
     HAL_GPIO_Init();
     HAL_I2C_Init();
     HAL_SPI_Init();
